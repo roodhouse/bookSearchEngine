cab394320d4179594a362972c9328befa5fdce22
roodhouse <rughjm@gmail.com>
2023-01-09 11:30:28 -0600

    added queries file

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks
+        }
+    }
+
+`
+
+
+
+
+// * `queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
